f6983ee707d09d5e92adbbd20176b1b2cb4868da
Andreas Fachathaler <faandi@fachi.net>
2020-02-20 14:06:35 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$0")"
+BUILD_NUMBER=${BUILD_BUILDNUMBER:-`date`}
+PUBLIC_DIR="$(realpath "$SCRIPT_DIR/public")"
+CACHE_DIR="$(realpath "$SCRIPT_DIR/.cache")"
+
+echo "building to $PUBLIC_DIR"
+
+rm $PUBLIC_DIR -Rf || exit 1
+rm $CACHE_DIR -Rf || exit 1
+git clone -b gh-pages `git remote get-url origin` $PUBLIC_DIR || exit 1
+npm install || exit 1
+npm run build || exit 1
+cd $PUBLIC_DIR || exit 1
+git add * || exit 1
+git commit -a -m "Release $BUILD_NUMBER" || exit 1
+git push || exit 1
+
+echo "published release named \"$BUILD_NUMBER\""
